f57628218e2a7bca5be64cfecefb6d795fcadb41
Erica Xie <eryka1999@gmail.com>
2020-10-09 14:52:28 -0700

    edited python script output

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     if x == "EE_180_DA_DB": 
         print("You are living in 2017")
     else: 
-        x = x + " - Best class ever"
+        x = x + " - Best class ever\n - sent from my RaspberryPi"
         print(x)
